2f8ebfed4f8b910dc58f2aeef91e5ce7e1bbd324
sotanmochi <stmczk@gmail.com>
2020-08-15 01:50:01 +0900

    Add low pass filter

--- Assets/AzureKinect4Unity/Examples/OrientationEstimation/AzureKinectOrientationEstimation.cs
+++ Assets/AzureKinect4Unity/Examples/OrientationEstimation/AzureKinectOrientationEstimation.cs
@@ -11,6 +11,9 @@ namespace AzureKinect4Unity
         [SerializeField] int _DeviceNumber = 0;
 
         AzureKinectSensor _KinectSensor;
+        ExponentialSmoothingLowPassFilter _LowPassFilter;
+        float [] _Accel = new float[3];
+        float [] _AccelOut = new float[3];
 
         PointCloudMesh _PointCloudMesh;
         Texture2D _ColorImageTexture;
@@ -25,6 +28,8 @@ namespace AzureKinect4Unity
                 {
                     Debug.Log("ColorResolution: " + _KinectSensor.ColorImageWidth + "x" + _KinectSensor.ColorImageHeight);
                     Debug.Log("DepthResolution: " + _KinectSensor.DepthImageWidth + "x" + _KinectSensor.DepthImageHeight);
+
+                    _LowPassFilter = new ExponentialSmoothingLowPassFilter((uint)_Accel.Length, 0.05f);
 
                     _ColorImageTexture = new Texture2D(_KinectSensor.ColorImageWidth, _KinectSensor.ColorImageHeight, TextureFormat.BGRA32, false);
 
@@ -60,6 +65,14 @@ namespace AzureKinect4Unity
                 }
 
                 System.Numerics.Vector3 accel = _KinectSensor.ImuSample.AccelerometerSample;
+                _Accel[0] = accel.X;
+                _Accel[1] = accel.Y;
+                _Accel[2] = accel.Z;
+                _LowPassFilter.Apply(_Accel, ref _AccelOut);
+                accel.X = _AccelOut[0];
+                accel.Y = _AccelOut[1];
+                accel.Z = _AccelOut[2];
+
                 UnityEngine.Vector3 kinectOrientation = OrientationEstimator.EstimateFromAccelerometerForUnity(accel);
 
                 this.transform.eulerAngles = kinectOrientation;
---
+++ Assets/AzureKinect4Unity/Scripts/ExponentialSmoothingLowPassFilter.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Soichiro Sugimoto.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace AzureKinect4Unity
+{
+    public class ExponentialSmoothingLowPassFilter
+    {
+        float[] _LastOutput;
+        float _Weight;
+
+        public ExponentialSmoothingLowPassFilter(uint vectorSize, float weight = 0.1f)
+        {
+            _LastOutput = new float[vectorSize];
+            _Weight = weight;
+        }
+
+        public bool Apply(in float[] input, ref float[] output)
+        {
+            if ((input.Length != output.Length)
+            ||  (input.Length != _LastOutput.Length))
+            {
+                return false;
+            }
+
+            for (int k = 0; k < input.Length; k++)
+            {
+                output[k] = _Weight * input[k] + (1.0f - _Weight) * _LastOutput[k];
+                _LastOutput[k] = output[k];
+            }
+
+            return true;
+        }
+    }
+}
